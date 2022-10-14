66de4e0fe15a8ee74c5de0bd129ed00c7dfb59d1
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-10-14 23:08:27 +0300

    Initial Commit

---
+++ listApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listApp
+//
+//  Created by Ali Görkem Aksöz on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
